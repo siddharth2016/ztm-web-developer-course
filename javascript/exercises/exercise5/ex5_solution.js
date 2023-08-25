a45ca51f544d7c5bd4c12bd48bdb174a108e0ebc
siddharth2016 <siddharthchandragzb@gmail.com>
2023-08-25 16:35:18 +0530

    solve JS exercise5

---
+++ javascript/exercises/exercise5/ex5_solution.js
@@ -0,0 +1,33 @@
+function checkDriverAgr() {
+    var age = prompt("What is your age?");
+
+    if (Number(age) < 18) {
+        alert("Sorry, you are too yound to drive this car. Powering off");
+    } else if (Number(age) > 18) {
+        alert("Powering On. Enjoy the ride!");
+    } else if (Number(age) === 18) {
+        alert("Congratulations on your first year of driving. Enjoy the ride!");
+    }
+}
+
+var canDrive = function() {
+    var age = prompt("What is your age?");
+
+    if (Number(age) < 18) {
+        alert("Sorry, you are too yound to drive this car. Powering off");
+    } else if (Number(age) > 18) {
+        alert("Powering On. Enjoy the ride!");
+    } else if (Number(age) === 18) {
+        alert("Congratulations on your first year of driving. Enjoy the ride!");
+    }
+}
+
+function checkDriverAge_WithArguments(age) {
+    if (Number(age) < 18) {
+        console.log("Sorry, you are too yound to drive this car. Powering off");
+    } else if (Number(age) > 18) {
+        console.log("Powering On. Enjoy the ride!");
+    } else if (Number(age) === 18) {
+        console.log("Congratulations on your first year of driving. Enjoy the ride!");
+    }
+}
