7d1df69bde6b43e53d2b7cb8863dd8a677dbe08a
siddharth2016 <siddharthchandragzb@gmail.com>
2023-08-25 22:42:30 +0530

    adding more users

--- javascript/newsfeed/script.js
+++ javascript/newsfeed/script.js
@@ -2,6 +2,14 @@ var database = [
     {
         username: "Siddharth",
         password: "password"
+    },
+    {
+        username: "Abhishek",
+        password: "password"
+    },
+    {
+        username: "Tanuja",
+        password: "password"
     }
 ];
 
@@ -17,8 +25,18 @@ var newsFeeds = [
 var user = prompt("Please enter your username: ");
 var pass = prompt("Please enter your password: ");
 
+function checkUser(user, pass) {
+    for(var i=0; i<database.length; i++) {
+        if(user === database[i].username && pass === database[i].password) {
+            console.log("Congratulations, successfully logged in!");
+            return true;
+        }
+    }
+    return false;
+}
+
 function signIn(user, pass) {
-    if(user === database[0].username && pass === database[0].password) {
+    if(checkUser(user, pass)) {
         console.log("Congratulations, successfully logged in!");
         console.log(newsFeeds);
     }
