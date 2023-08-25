3a79fac8be93e1e50aafd85edf2e203a1f409876
siddharth2016 <siddharthchandragzb@gmail.com>
2023-08-25 21:53:59 +0530

    solve JS exercise 7

---
+++ javascript/exercises/exercise7/ex7_solution.js
@@ -0,0 +1,21 @@
+var someObj = {
+    username: "SomeName",
+    pass: "SomePassword"
+};
+
+var dbArray = [someObj];
+
+var newsFeed = [
+    {
+        username: "some1",
+        timeline: "timeline1"
+    },
+    {
+        username: "some2",
+        timeline: "timeline2"
+    },
+    {
+        username: "some3",
+        timeline: "timeline3"
+    }
+];
