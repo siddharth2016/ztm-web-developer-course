868ba3bbaca3f388e4aced98a8d45926e09fb48f
siddharth2016 <siddharthchandragzb@gmail.com>
2023-08-27 14:14:18 +0530

    solve JS exercise8

---
+++ javascript/exercises/exercise8/dom-solution.js
@@ -0,0 +1,20 @@
+document.getElementsByTagName("a");
+/*
+HTMLCollection(25) [a.gb_y, a.gb_y, a.gb_d, a.gb_d.gb_xa.gb_A, a#sbfblt.oBa0Fe.aciXEb, a, a, a, a, a, a, a, a, a, a.pHiOh, a.pHiOh, a.pHiOh, a.pHiOh, a.pHiOh, a.pHiOh, a, a, a, a, a, sbfblt: a#sbfblt.oBa0Fe.aciXEb]
+*/
+
+document.getElementsByClassName("o3j99");
+/*
+HTMLCollection(5) [div.o3j99.n1xJcf.Ne6nSd, div.o3j99.LLD4me.yr19Zb.LS8OJ, div.o3j99.ikrT4e.om7nvf, div.o3j99.qarstb, div.o3j99.c93Gbe]
+*/
+
+document.getElementsByClassName("o3j99")[0].getAttribute("id");
+document.getElementById("");
+
+document.querySelector("h1");
+document.querySelector("li");
+document.querySelectorAll("li");
+
+document.getElementById("someId").getAttribute("width");
+document.getElementsByTagName("img")[1].setAttribute("height", "100");
+
