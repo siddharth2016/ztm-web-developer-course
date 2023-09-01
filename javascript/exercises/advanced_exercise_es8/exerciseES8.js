d019e7603f3f98b743f91a34b33f0cf38921f11a
siddharth2016 <siddharthchandragzb@gmail.com>
2023-09-01 18:35:51 +0530

    reformatting

--- javascript/exercises/advanced_exercise_es8/exerciseES8.js
+++ javascript/exercises/advanced_exercise_es8/exerciseES8.js
@@ -19,7 +19,6 @@ console.log(rabbit.padStart(8));
 // console.log(turtle);
 // console.log(rabbit);
 
-
 // #2) What happens when you run turtle.trim().padEnd(9, '=') on the turtle variable
 // Read about what the second parameter does in padEnd and padStart
 turtle = turtle.trim().padEnd(9, '=');
